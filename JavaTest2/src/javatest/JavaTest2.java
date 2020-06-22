bf51f2ffae39dc2415a64ee22bf6f36b44599865
waji444 <67175094+waji444@users.noreply.github.com>
2020-06-22 16:10:39 -0400

    Add files via upload

---
+++ JavaTest2/src/javatest/JavaTest2.java
@@ -0,0 +1,26 @@
+package javatest;
+
+import java.util.Scanner;
+
+public class JavaTest2 {
+
+	public static void main(String[] args) {
+	//	2.Take a number from user and Print if the number is less than 200 and even number 
+    //otherwise print invalid number using Logical Operator
+		int a;
+	Scanner ab=new Scanner(System.in);
+	System.out.println("enter the number");
+	a=ab.nextInt();
+		
+		
+	if(a<200 & a%2==0) {
+		System.out.println("true"); }
+	else 
+		System.out.println("invalid number");
+		
+		
+		
+		
+	}
+
+}
